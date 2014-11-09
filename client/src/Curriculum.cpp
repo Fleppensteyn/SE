e8865d6fb078f26eaeaa8f57880c61e62167a00e
jacolink <jacob.jonkman@gmail.com>
2014-11-09 16:46:08 +0100

    Datastructuur voor een curriculum geïmplementeerd

--- client/src/Curriculum.cpp
+++ client/src/Curriculum.cpp
@@ -4,13 +4,40 @@
 */
 
 #include "Curriculum.h"
-
 Curriculum::Curriculum(wxBookCtrl *curricula)
       :wxPanel(curricula, wxID_ANY, wxPoint(50,100), wxSize(500, 500))
 {
-  
+  numberOfCourses = 0;
+  /*Hier kunnen we het courses array doorlopen en iedere entry een leeg vak maken voor de netheid*/
 }
 
 Curriculum::~Curriculum(){
   
+}
+
+void Curriculum::AddCourse(/*Course newCourse*/){
+	numberOfCourses++;
+	/*courses[numberOfCourses] = newCourse; */
+}
+
+void Curriculum::DeleteCourse(/*Course toDelete*/){
+	/*Verwijder het vak, afhankelijk van Course-datastructuur*/
+	numberOfCourses--;
+}
+
+void Curriculum::ShowCurriculum(){
+	/* 	TODO:
+			loop courses array door
+			print een course in een vak met nader te bepalen lengte (bijvoorbeeld 150 met spacer van 25, totaal moet 500 zijn)
+			zet een spacer om een klein stukje wit toe te voegen na ieder vak. Doe dit ook tussen regels
+			scroll?
+	*/
+}
+
+void Curriculum::UpdateCourse(/*Course toUpdate*/){
+	/*	TODO:
+			Zoek het vak in het curriculum
+			Gevonden: Vervang de oude entry met toUpdate
+			Niet gevonden: print foutmelding
+	*/
 }
--- client/src/Curriculum.h
+++ client/src/Curriculum.h
@@ -13,6 +13,10 @@
 #endif
 
 #include "wx/bookctrl.h"
+#include <string>
+
+//There can be no more than 50 courses in a curriculum
+const int MAXCOURSES = 50;
 
 //Contains all gui elements and functionality of a single curriculum
 class Curriculum : public wxPanel
@@ -22,9 +26,23 @@ public:
   // curricula - The parent tab control
   Curriculum(wxBookCtrl *curricula);
   virtual ~Curriculum();
-
+  
+  //Adds a course to this curriculum
+  void AddCourse(/*Course newCourse*/);
+  
+  //Deletes a course from this curriculum
+	void DeleteCourse(/*Course toDelete*/);
+  
+  //Prints this curriculum in the overview box
+  void ShowCurriculum();
+  
+  //Updates the contents of a course in this curriculum. Used after editting a course in the course catalogue
+	void UpdateCourse(/*Course toUpdate*/);
+	
 private:
-
+	std::string curName; 						//Name of the curriculum
+	/*Course courses[MAXCOURSES];*/ //Array with all the courses in this curriculum
+	int numberOfCourses; 						//The number of courses in this curriculum
   //wxDECLARE_EVENT_TABLE();
 };
 
